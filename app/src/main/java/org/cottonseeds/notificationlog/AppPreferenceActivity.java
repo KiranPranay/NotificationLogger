95c99829d9e0ff34e36c22fa7ee79d1bb0bc6acd
kiranpranay <pranaybannu12@gmail.com>
2022-12-17 15:04:10 +0530

    Rebrand to org.cottonseeds.notificationlog
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

---
+++ app/src/main/java/org/cottonseeds/notificationlog/AppPreferenceActivity.java
@@ -0,0 +1,16 @@
+package org.cottonseeds.notificationlog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.cottonseeds.notificationlog.R;
+
+public class AppPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_preference);
+    }
+}
